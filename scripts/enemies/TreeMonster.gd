5b6af4aa5930c17ec953aa677a955e84e0f8773e
Guilherme <gui.crb0@gmail.com>
2022-04-01 16:01:08 -0300

    randomizing attacks

--- scripts/enemies/TreeMonster.gd
+++ scripts/enemies/TreeMonster.gd
@@ -1,13 +1,18 @@
 extends StaticBody2D
 
+var numberGenerator
+var inAttackArea = false
+
 func _ready():
 	$AnimationPlayer.play("sleeping")
+	numberGenerator = RandomNumberGenerator.new()
 
 func _on_AttackArea_body_entered(body):
-	$AnimationPlayer.play("areaAttack")
-	$AnimationPlayer.queue("idle")
+	inAttackArea = true
+	attack()
 
 func _on_AttackArea_body_exited(body):
+	inAttackArea = false
 	$AnimationPlayer.queue("idle")
 
 func _on_SightArea_body_entered(body):
@@ -15,3 +20,17 @@ func _on_SightArea_body_entered(body):
 
 func _on_SightArea_body_exited(body):
 	$AnimationPlayer.play_backwards("wakingUp")
+
+func attack():
+	if inAttackArea:
+		numberGenerator.randomize()
+		var attackNum = numberGenerator.randi_range(0,1)
+		var attackType
+		if attackNum == 0:
+			attackType = "areaAttack"
+		elif attackNum == 1:
+			attackType = "stingAttack"
+		if $AnimationPlayer.current_animation == "idle":
+			$AnimationPlayer.play(attackType)
+		else:
+			$AnimationPlayer.queue(attackType)
