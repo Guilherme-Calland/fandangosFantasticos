e45f15dba7acc057664ef6401136334b85bd6e67
Guilherme <gui.crb0@gmail.com>
2022-03-31 22:13:30 -0300

    tree monster area attack

--- scripts/enemies/TreeMonster.gd
+++ scripts/enemies/TreeMonster.gd
@@ -12,14 +12,10 @@ func playIdle():
 func playAreaAttack():
 	$AnimationPlayer.play("areaAttack")
 
-func playReady2():
-	$AnimationPlayer.play("ready2")
-
-
 func _on_Area2D_body_entered(body):
-	print('collision')
-	if not $AnimationPlayer.current_animation == "ready" && not $AnimationPlayer.current_animation == "ready2":
-			$AnimationPlayer.play("gettingReady")
+#	if not $AnimationPlayer.current_animation == "ready":
+#			$AnimationPlayer.play("gettingReady")
+	playAreaAttack()
 
 func _on_Area2D_body_exited(body):
 	if not $AnimationPlayer.current_animation == "idle":
---
+++ scripts/levels/testLevel/TestLevel2.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+export var gravity = 10
+export var friction = 0.1
+export var airResistance = 0.1
+
+var worldBundle
+
+func _process(delta):
+	packWorldBundle()
+	$Player1.run(worldBundle)
+
+func packWorldBundle():
+	worldBundle = {
+	'gravity' : gravity,
+	'friction' : friction,
+	'airResistance' : airResistance
+	}
+
+
