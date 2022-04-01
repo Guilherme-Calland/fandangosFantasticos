9a65748c7507edcfcdb38e746664359570f3b14a
Guilherme <gui.crb0@gmail.com>
2022-04-01 00:09:50 -0300

    animating area attack

--- scripts/enemies/TreeMonster.gd
+++ scripts/enemies/TreeMonster.gd
@@ -1,22 +1,16 @@
 extends StaticBody2D
 
 func _ready():
-	$AnimationPlayer.play("idle")
+	$AnimationPlayer.play("sleeping")
 
-func playReady():
-	$AnimationPlayer.play("ready")
-
-func playIdle():
-	$AnimationPlayer.play("idle")
-
-func playAreaAttack():
+func _on_AttackArea_body_entered(body):
 	$AnimationPlayer.play("areaAttack")
 
-func _on_Area2D_body_entered(body):
-#	if not $AnimationPlayer.current_animation == "ready":
-#			$AnimationPlayer.play("gettingReady")
-	playAreaAttack()
+func _on_AttackArea_body_exited(body):
+	$AnimationPlayer.play("idle")
 
-func _on_Area2D_body_exited(body):
-	if not $AnimationPlayer.current_animation == "idle":
-			$AnimationPlayer.play("backToIdle")
+func _on_SightArea_body_entered(body):
+	$AnimationPlayer.play("wakingUp")
+
+func _on_SightArea_body_exited(body):
+	$AnimationPlayer.play("backToSleep")
