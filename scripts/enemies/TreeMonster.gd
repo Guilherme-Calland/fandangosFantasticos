0459eea0d6bb336e5aece0d858cc3e3ad437f7eb
Guilherme <gui.crb0@gmail.com>
2022-04-01 14:28:53 -0300

    tree mosnter sting attack

--- scripts/enemies/TreeMonster.gd
+++ scripts/enemies/TreeMonster.gd
@@ -5,12 +5,13 @@ func _ready():
 
 func _on_AttackArea_body_entered(body):
 	$AnimationPlayer.play("areaAttack")
+	$AnimationPlayer.queue("idle")
 
 func _on_AttackArea_body_exited(body):
-	$AnimationPlayer.play("idle")
+	$AnimationPlayer.queue("idle")
 
 func _on_SightArea_body_entered(body):
 	$AnimationPlayer.play("wakingUp")
 
 func _on_SightArea_body_exited(body):
-	$AnimationPlayer.play("backToSleep")
+	$AnimationPlayer.play_backwards("wakingUp")
